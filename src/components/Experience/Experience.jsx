acd1bb59816b23daed21b0af573368f7fba29a1c
JhonnaFreire <jhonatan.freire@hotmail.com>
2023-07-31 20:03:44 -0500

    images

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,7 @@
 import "./Experience.css";
 import javascript from '/src/assets/img/Js.png'
 import html from '/src/assets/img/html.png'
-import css from '/src/assets/img/css'
+import css from '/src/assets/img/css.png'
 
 
 const Experience = () => {
@@ -9,11 +9,11 @@ const Experience = () => {
     <>
       <h2 className="experience__title" >Experience</h2>
       <div className="experience__logos">
-        <img src={javascript} alt="" />
+        <img src={javascript} />
         <h3 className="experience__subtitle">Javascript</h3>
-        <img src={html} alt="" />
+        <img src={html} />
         <h3 className="experience__subtitle">HTML</h3>
-        <img src={css} alt="" />
+        <img src={css} />
         <h3 className="experience__subtitle">CSS</h3>
       </div>
     </>
